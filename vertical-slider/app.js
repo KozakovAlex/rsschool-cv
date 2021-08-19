00cdd2fcdff7ec28c2cd12c265d474dce83fe509
KozakovAlex <72526330+KozakovAlex@users.noreply.github.com>
2021-08-19 23:10:49 +0300

    refactor: add function changeSlides to improve code

--- vertical-slider/app.js
+++ vertical-slider/app.js
@@ -1,36 +1,40 @@
+const container = document.querySelector('.container');
+
 const leftSlider = document.querySelector('.left-slider');
 const rightSlider = document.querySelector('.right-slider');
 const leftSliderItems = document.querySelectorAll('.left-slider__item');
 let currentPos = 0;
 let slidesCount = leftSliderItems.length;
+
+leftSlider.style.top = `${-(slidesCount - 1) * 100}vh`;
 // console.log(slidesCount);
 
 
 const downButton = document.querySelector('.down-button');
 const upButton = document.querySelector('.up-button');
 
-upButton.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  if (currentPos == 3) {
-    currentPos = 0;
-    rightSlider.style.transform = `translateY(-${currentPos * 100}vh)`;
-    leftSlider.style.transform = `translateY(${-300 + currentPos * 100}vh)`;
-    return;
-  }
-  currentPos += 1;
-  rightSlider.style.transform = `translateY(-${currentPos * 100}vh)`;
-  leftSlider.style.transform = `translateY(${-300 + currentPos * 100}vh)`;
+upButton.addEventListener('click', () => {
+  changeSlides('up');
 });
 
-downButton.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  if (currentPos == 0) {
-    currentPos = 3;
-    rightSlider.style.transform = `translateY(-${currentPos * 100}vh)`;
-    leftSlider.style.transform = `translateY(${-300 + currentPos * 100}vh)`;
-    return;
-  }
-  currentPos -= 1;
-  rightSlider.style.transform = `translateY(-${currentPos * 100}vh)`;
-  leftSlider.style.transform = `translateY(${-300 + currentPos * 100}vh)`;
+downButton.addEventListener('click', () => {
+  changeSlides('down');
 });
+
+function changeSlides (direction) {
+  if (direction === 'up') {
+    currentPos++;
+    if (currentPos === slidesCount) {
+      currentPos = 0;
+    }
+  } else if (direction === 'down') {
+    currentPos--;
+    if (currentPos < 0) {
+      currentPos = slidesCount - 1;
+    }
+  }
+
+  rightSlider.style.transform = `translateY(-${currentPos * 100}vh)`;
+  leftSlider.style.transform = `translateY(${currentPos * 100}vh)`;
+
+}
