67e3030f1b13ac104f0039ad05ccfb0505a5fb58
KozakovAlex <72526330+KozakovAlex@users.noreply.github.com>
2021-08-26 23:45:57 +0300

    feat: finish styles, start JS

---
+++ whack-a-mole/index.js
@@ -0,0 +1,5 @@
+const scoreResult = document.querySelector('.score__result');
+const holes = document.querySelectorAll('.hole');
+const moles = document.querySelectorAll('.mole');
+
+const btnStart = document.querySelector('.btn-start');
