4c6b8384de9ca5438ddaf918f117c391354971cc
KozakovAlex <72526330+KozakovAlex@users.noreply.github.com>
2021-08-27 20:39:35 +0300

    feat: start JS

--- whack-a-mole/index.js
+++ whack-a-mole/index.js
@@ -3,3 +3,9 @@ const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
 
 const btnStart = document.querySelector('.btn-start');
+
+function randTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+console.log(randTime(200, 1000));
