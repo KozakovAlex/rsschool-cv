ee3004b601bad4128161e5bcb7f9f8314c9d67b4
KozakovAlex <72526330+KozakovAlex@users.noreply.github.com>
2021-09-21 00:00:18 +0300

    feat: finish task

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ const mainHeaderWrapper = document.querySelector('.main-header__wrapper');
 const upButton = document.querySelector('.up-button');
 const codeTitleButton = document.querySelector('.code-title-button');
 const codeExample = document.querySelector('.code-example');
+const mainHeaderBurger = document.querySelector('.main-header__burger');
+const burgerLineTop = document.querySelector('.burger-line-top');
+const burgerLineMiddle = document.querySelector('.burger-line-middle');
+const burgerLineBottom = document.querySelector('.burger-line-bottom');
+const mainHeaderMenuBurger = document.querySelector('.main-header__menu-burger');
 
 
 (function () {
@@ -26,5 +31,12 @@ upButton.addEventListener('click', () => {
 codeTitleButton.addEventListener('click', () => {
   codeExample.classList.toggle('code-example_disabled');
   codeExample.classList.toggle('code-example_active');
+})
+
+mainHeaderBurger.addEventListener('click', () => {
+  burgerLineTop.classList.toggle('burger-line-top_active');
+  burgerLineMiddle.classList.toggle('burger-line-middle_active');
+  burgerLineBottom.classList.toggle('burger-line-bottom_active');
+  mainHeaderMenuBurger.classList.toggle('main-header__menu-burger_active');
 })
 
