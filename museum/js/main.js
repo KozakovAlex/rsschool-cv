b835e46afdccdc5bd62b3a5bc2e979ae53cdcc98
KozakovAlex <72526330+KozakovAlex@users.noreply.github.com>
2021-09-28 00:05:17 +0300

    feat: finish sections Video, Gallery, Footer, parallax

--- museum/js/main.js
+++ museum/js/main.js
@@ -23,3 +23,36 @@ const mainHeader = document.querySelector('.main-header');
     }
   }
 }());
+
+
+const picturesSrc = [
+  './img/galery/galery1.jpg',
+  './img/galery/galery2.jpg',
+  './img/galery/galery3.jpg',
+  './img/galery/galery4.jpg',
+  './img/galery/galery5.jpg',
+  './img/galery/galery6.jpg',
+  './img/galery/galery7.jpg',
+  './img/galery/galery8.jpg',
+  './img/galery/galery9.jpg',
+  './img/galery/galery10.jpg',
+  './img/galery/galery11.jpg',
+  './img/galery/galery12.jpg',
+  './img/galery/galery13.jpg',
+  './img/galery/galery14.jpg',
+  './img/galery/galery15.jpg'
+];
+const shufflepicturesSrc = shuffle(picturesSrc);
+
+const galleryPictures = document.querySelectorAll('.gallery__pictures-pic');
+for (let i = 0; i < galleryPictures.length; i++) {
+  galleryPictures[i].src = shufflepicturesSrc[i];
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
